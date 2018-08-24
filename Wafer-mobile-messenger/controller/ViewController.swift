91d69dd6baec3ef0a2d1e6127880d157ded6a482
Thomas <msamoht16@gmail.com>
2018-08-24 11:33:13 -0300

    add project basis

---
+++ Wafer-mobile-messenger/controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Wafer-mobile-messenger
+//
+//  Created by Thomás Marques Brandão Reis on 24/08/2018.
+//  Copyright © 2018 Thomás Marques Brandão Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
