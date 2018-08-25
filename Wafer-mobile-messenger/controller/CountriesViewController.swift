485b0eef0affc250903e73053422f71480e2bd53
Thomas <msamoht16@gmail.com>
2018-08-24 21:08:13 -0300

    Print list with country information

--- Wafer-mobile-messenger/controller/CountriesViewController.swift
+++ Wafer-mobile-messenger/controller/CountriesViewController.swift
@@ -8,10 +8,45 @@
 
 import UIKit
 
-class CountriesViewController: UIViewController {
+class CountriesViewController: UITableViewController {
 
+    var coutries = [Country]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getCountries()
     }
+    
+    // MARK: - TableView
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coutries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let country = coutries[indexPath.row]
+        let currencyName = country.currencies?.first?.name ?? ""
+        let languageName = country.languages?.first?.name ?? ""
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellCountry") as! UITableViewCell
+        cell.textLabel?.text = country.name
+        cell.detailTextLabel?.text = "\(currencyName) - \(languageName)"
+        return cell
+    }
+    
+    // MARK: - Functions
+    
+    func getCountries() {
+        DataManager().getCountries { (countries) in
+            self.coutries = countries
+            OperationQueue.main.addOperation {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
 
 }
