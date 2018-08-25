76d72ca63040332ee0651b7797097d1d0eeb83dc
Thomas <msamoht16@gmail.com>
2018-08-24 21:45:23 -0300

    Add connection verification when performing request

---
+++ Wafer-mobile-messenger/controller/BaseTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  BaseTableViewController.swift
+//  Wafer-mobile-messenger
+//
+//  Created by Thomás Marques Brandão Reis on 24/08/2018.
+//  Copyright © 2018 Thomás Marques Brandão Reis. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewController: UITableViewController {
+    
+    private let label = UILabel()
+    private let loadingView = UIView()
+    private let loadingLabel = UILabel()
+    private let spinner = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.separatorStyle = .none
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func show(message: String, refreshAction: Selector) {
+        label.removeFromSuperview()
+        stopLoading()
+        let width: CGFloat = 250
+        let height: CGFloat = 30
+        let x = (tableView.frame.width / 2) - (width / 2)
+        let y = (tableView.frame.height / 2) - (height / 2) - (navigationController?.navigationBar.frame.height)!
+        label.frame = CGRect(x: x, y: y, width: width, height: height)
+        label.text = message
+        label.textAlignment = .center
+        label.textColor = .gray
+        let tap = UITapGestureRecognizer(target: self, action: refreshAction)
+        label.isUserInteractionEnabled = true
+        label.addGestureRecognizer(tap)
+        tableView.addSubview(label)
+    }
+    
+    func startLoading() {
+        let width: CGFloat = 120
+        let height: CGFloat = 30
+        let x = (tableView.frame.width / 2) - (width / 2)
+        let y = (tableView.frame.height / 2) - (height / 2) - (navigationController?.navigationBar.frame.height)!
+        loadingView.frame = CGRect(x: x, y: y, width: width, height: height)
+        loadingLabel.textColor = .gray
+        loadingLabel.textAlignment = .center
+        loadingLabel.text = Constants.loading
+        loadingLabel.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
+        spinner.activityIndicatorViewStyle = .gray
+        spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        spinner.startAnimating()
+        loadingView.addSubview(spinner)
+        loadingView.addSubview(loadingLabel)
+        tableView.addSubview(loadingView)
+    }
+    
+    func stopLoading() {
+        label.removeFromSuperview()
+        spinner.stopAnimating()
+        spinner.isHidden = true
+        loadingLabel.isHidden = true
+        spinner.removeFromSuperview()
+        loadingLabel.removeFromSuperview()
+        loadingView.removeFromSuperview()
+    }
+
+    
+}
--- Wafer-mobile-messenger/controller/CountriesViewController.swift
+++ Wafer-mobile-messenger/controller/CountriesViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class CountriesViewController: UITableViewController {
+class CountriesViewController: BaseTableViewController {
 
-    var coutries = [Country]()
+    var countries = [Country]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getCountries()
+        getCountries(sender: self)
     }
     
     // MARK: - TableView
@@ -24,25 +24,35 @@ class CountriesViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return coutries.count
+        return countries.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let country = coutries[indexPath.row]
+        let country = countries[indexPath.row]
         let currencyName = country.currencies?.first?.name ?? ""
         let languageName = country.languages?.first?.name ?? ""
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellCountry") as! UITableViewCell
         cell.textLabel?.text = country.name
         cell.detailTextLabel?.text = "\(currencyName) - \(languageName)"
+        cell.accessoryType = .disclosureIndicator
         return cell
     }
     
     // MARK: - Functions
     
-    func getCountries() {
+    @objc func getCountries(sender: AnyObject) {
+        if !Reachability.isConnectedToNetwork() {
+            show(message: Constants.no_connection, refreshAction: #selector(CountriesViewController.getCountries))
+            return
+        }
+        startLoading()
         DataManager().getCountries { (countries) in
-            self.coutries = countries
+            self.countries = countries
             OperationQueue.main.addOperation {
+                self.stopLoading()
+                if self.countries.count == 0 {
+                    self.show(message: Constants.empty_countries_list, refreshAction: #selector(CountriesViewController.getCountries))
+                }
                 self.tableView.reloadData()
             }
         }
